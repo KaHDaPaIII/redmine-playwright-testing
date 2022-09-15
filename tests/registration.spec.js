ead111872837fa7adc83c540bdabefee7b8c2a84
Maksym <wfbykahdapaiii@gmail.com>
2022-09-15 18:24:47 +0300

    Edited the code a bit

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -4,9 +4,7 @@ const { randomString } = require('../my-functions/my-function');
 
 test('Register with valid random generated credentials.', async ({ page }) => {
     await page.goto('https://www.redmine.org/');
-    
     await page.locator('[href="/account/register"]').click();
-
     await page.locator('#user_login').fill(randomString(8));
     const password = randomString(16);
     await page.locator('#user_password').fill(password);
@@ -14,19 +12,14 @@ test('Register with valid random generated credentials.', async ({ page }) => {
     await page.locator('#user_firstname').fill(randomString(8));
     await page.locator('#user_lastname').fill(randomString(8));
     await page.locator('#user_mail').fill(emails.generateEmail());
-
     await page.locator('[type="submit"]').click();
-
     await expect(page).toHaveURL('https://www.redmine.org/login');
     await expect(page.locator('#flash_notice')).toBeVisible();
 })
 
 test('Register with blank form.', async ({ page }) => {
     await page.goto('https://www.redmine.org/');
-    
     await page.locator('[href="/account/register"]').click();
-
     await page.locator('[type="submit"]').click();
-
     await expect(page.locator('#errorExplanation')).toBeVisible();
 })
