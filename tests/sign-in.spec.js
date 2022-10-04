44c2765ce285ddd51f702363910080dcfcc2929d
Maksym <wfbykahdapaiii@gmail.com>
2022-10-04 18:12:16 +0300

    Add .env file instead of credentials.json

--- tests/sign-in.spec.js
+++ tests/sign-in.spec.js
@@ -1,19 +1,18 @@
 const { test, expect } = require('@playwright/test');
 const { RedmineHomePage } = require('./pageobjects/redmine-home-page.js');
 const { RedmineLoginPage } = require('./pageobjects/redmine-login-page.js');
-const { login, password } = require('./my-functions/credentials.json');
 const { UserCredentials } = require('./my-functions/generate-credentials.js');
 
-test('Sign in with valid credentials.', async ({ page }) => {
+test.only('Sign in with valid credentials.', async ({ page }) => {
     const homePage = new RedmineHomePage(page);
     const loginPage = new RedmineLoginPage(page);
     
     await homePage.goto();
     await homePage.clickSignIn();
-    await loginPage.login(login, password);
+    await loginPage.login(process.env.TESTACCOUNT_LOGIN, process.env.TESTACCOUNT_PASSWORD);
     
     await expect(page.locator('#loggedas')).toBeVisible();
-    await expect(page.locator('#loggedas > a')).toHaveText(login);
+    await expect(page.locator('#loggedas > a')).toHaveText(process.env.TESTACCOUNT_LOGIN);
 })
 
 test('Sign in with not valid (random generated) credentials.', async ({ page }) => {
@@ -34,7 +33,7 @@ test('“Stay logged in” is working', async ({ page }) => {
     
     await homePage.goto();
     await homePage.clickSignIn();
-    await loginPage.login(login, password, true);
+    await loginPage.login(process.env.TESTACCOUNT_LOGIN, process.env.TESTACCOUNT_PASSWORD, true);
     const newPage = await page.context().newPage();
     await page.close();
     await newPage.goto('https://www.redmine.org/');
